f455edcfae7b8f03eddbb8d0105f190910900837
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2021-10-18 18:11:49 +0200

    Added all buffs to respective class/spec in SpecList

--- src/SpecList.js
+++ src/SpecList.js
@@ -1,214 +1,214 @@
 const SpecList = [
     {
   
-      text:"Blood Death Knight",
+      text:'Blood Death Knight',
       image:require('./images/dk_blood.png').default,
       color:'red',
-      buffs:'',
+      buffs:["Agility and Strength", "Attack Power %"]
     },
     {    
   
-      text:"Unholy Death Knight",
+      text:'Unholy Death Knight',
       image:require('./images/dk_unholy.png').default,
       color:'red',
-      buffs:'',
+      buffs:["Agility and Strength", "Spell Damage %","Disease Damage"]
     },
     {
   
-      text:"Frost Death Knight",
+      text:'Frost Death Knight',
       image:require('./images/dk_frost.png').default,
       color:'red',
-      buffs:'',
+      buffs:["Agility and Strength", "Melee Haste"],
     },
     {    
   
-      text:"Balance Druid",
+      text:'Balance Druid',
       image:require('./images/druid_balance.png').default,
       color:'orange',
-      buffs:'',
+      buffs:["Haste %", "Spell Crit", "Motw", "Spell Damage %", "Spell Hit"],
     },
     {    
   
-      text:"Feral Druid",
+      text:'Feral Druid',
       image:require('./images/druid_feral.png').default,
       color:'orange',
-      buffs:'',
+      buffs:["Melee Crit", "Motw","Armor Reduction(Minor)"],
     },
     {    
   
-      text:"Restoration Druid",
+      text:'Restoration Druid',
       image:require('./images/druid_restoration.png').default,
       color:'orange',
-      buffs:'',
+      buffs:["Healing Received %", "Motw"]
     },
     {
   
-      text:"Beast Mastery Hunter",
+      text:'Beast Mastery Hunter',
       image:require('./images/hunter_beastmastery.png').default,
       color:'green',
-      buffs:'',
+      buffs:["Damage %" ,"Armor Reduction(Minor)"],
     },
     {
   
-      text:"Marksman Hunter",
+      text:'Marksman Hunter',
       image:require('./images/hunter_marksman.png').default,
       color:'green',
-      buffs:'',
+      buffs:["Attack Power %", "Armor Reduction(Minor)"],
     },
     {
   
-      text:"Survival Hunter",
+      text:'Survival Hunter',
       image:require('./images/hunter_survival.png').default,
       color:'green',
-      buffs:'',
+      buffs:["Replenishment", "Armor Reduction(Minor)"],
     },
     {    
   
-      text:"Arcane Mage",
+      text:'Arcane Mage',
       image:require('./images/mage_arcane.png').default,
       color:'teal',
-      buffs:'',
+      buffs:["Damage %", "Intellect"],
     },
     {
   
-      text:"Fire Mage",
+      text:'Fire Mage',
       image:require('./images/mage_fire.png').default,
       color:'teal',
-      buffs:'',
+      buffs:["Intellect", "Spell Crit"],
     },
     {    
   
-      text:"Frost Mage",
+      text:'Frost Mage',
       image:require('./images/mage_frost.png').default,
       color:'teal',
-      buffs:'',
+      buffs:["Intellect","Replenishment" ,"Spell Crit"],
     },
     {
   
-      text:"Holy Paladin",
+      text:'Holy Paladin',
       image:require('./images/paladin_holy.png').default,
       color:'pink',
-      buffs:'',
+      buffs:["Attack Power", "Mana per 5", "Stats 10%"],
     },
     {
   
-      text:"Protection Paladin",
+      text:'Protection Paladin',
       image:require('./images/paladin_protection.png').default,
       color:'pink',
-      buffs:'',
+      buffs:["Attack Power", "Damage Reduction %", "Healing Received %","Mana per 5", "Stats 10%" ,"Crit Strike %"],
     },
     {
   
-      text:"Retribution Paladin",
+      text:'Retribution Paladin',
       image:require('./images/paladin_retribution.png').default,
       color:'pink',
-      buffs:'',
+      buffs:["Attack Power", "Damage %" ,"Haste %" ,"Mana per 5" ,"Replenishment", "Stats 10%", "Crit Strike %"],
     },
     {
   
-      text:"Discipline Priest",
+      text:'Discipline Priest',
       image:require('./images/priest_discipline.png').default,
       color:'gray',
-      buffs:'',
+      buffs:["Armor %","Damage Reduction %","Spirit","Stamina"],
     },
     {
   
-      text:"Holy Priest",
+      text:'Holy Priest',
       image:require('./images/priest_holy.png').default,
       color:'gray',
-      buffs:'',
+      buffs:["Spirit","Stamina","Armor %"],
     },
     {
   
-      text:"Shadow Priest",
+      text:'Shadow Priest',
       image:require('./images/priest_shadow.png').default,
       color:'gray',
-      buffs:'',
+      buffs:["Replenishment", "Spirit", "Stamina","Spell Hit"],
     },
     {
   
-      text:"Assassination Rogue",
+      text:'Assassination Rogue',
       image:require('./images/rogue_assassination.png').default,
       color:'yellow',
-      buffs:'',
+      buffs:["Armor Reduction(Major)","Crit Strike %"],
     },
     {
   
-      text:"Combat Rogue",
+      text:'Combat Rogue',
       image:require('./images/rogue_combat.png').default,
       color:'yellow',
-      buffs:'',
+      buffs:["Armor Reduction(Major)", "Phys Vulnerability"],
     },
     {
   
-      text:"Subtlety Rogue",
+      text:'Subtlety Rogue',
       image:require('./images/rogue_subtlety.png').default,
       color:'yellow',
-      buffs:'',
+      buffs:["Armor Reduction(Major)"],
     },
   
     {
   
-      text:"Elemental Shaman",
+      text:'Elemental Shaman',
       image:require('./images/shaman_elemental.png').default,
       color:'blue',
-      buffs:'',
+      buffs:["Agility and Strength", "Bloodlust", "Mana per 5", "Spell Crit", "Spell Haste", "Spellpower(Major)", "Crit Strike %","Spellpower(Minor)"],
     },
     {
   
-      text:"Enhancement Shaman",
+      text:'Enhancement Shaman',
       image:require('./images/shaman_enhancement.png').default,
       color:'blue',
-      buffs:'',
+      buffs:["Agility and Strength", "Attack Power %", "Bloodlust", "Mana per 5", "Melee Haste", "Spellpower(Minor)"],
     },
     {
   
-      text:"Restoration Shaman",
+      text:'Restoration Shaman',
       image:require('./images/shaman_restoration.png').default,
       color:'blue',
-      buffs:'',
+      buffs:["Agility and Strength", "Armor %" ,"Bloodlust", "Mana per 5" ,"Spellpower(Minor)"],
     },
     {
   
-      text:"Arms Warrior",
+      text:'Arms Warrior',
       image:require('./images/warrior_arms.png').default,
       color:'brown',
-      buffs:'',
+      buffs:["Attack Power","Health","Armor Reduction(Major)","Phys Vulnerability"],
     },
     {
   
-      text:"Fury Warrior",
+      text:'Fury Warrior',
       image:require('./images/warrior_fury.png').default,
       color:'brown',
-      buffs:'',
+      buffs:["Attack Power", "Health", "Melee Crit", "Armor Reduction(Major)"],
     },
     {
   
-      text:"Protection Warrior",
+      text:'Protection Warrior',
       image:require('./images/warrior_protection.png').default,
       color:'brown',
-      buffs:'',
+      buffs:["Attack Power", "Armor Reduction(Major)", "Health"],
     },
     {
   
-      text:"Affliction Warlock",
+      text:'Affliction Warlock',
       image:require('./images/warlock_affliction.png').default,
       color:'purple',
-      buffs:'',
+      buffs:["Intellect", "Spirit", "Armor Reduction(Minor)", "Spell Crit", "Spell Damage %"],
     },
     {
   
-      text:"Demonology Warlock",
+      text:'Demonology Warlock',
       image:require('./images/warlock_demonology.png').default,
       color:'purple',
-      buffs:'',
+      buffs:["Spellpower", "Armor Reduction(Minor)", "Spell Crit", "Spell Damage %"],
     },
     {
    
-      text:"Destruction Warlock",
+      text:'Destruction Warlock',
       image:require('./images/warlock_destruction.png').default,
       color:'purple',
-      buffs:'',
+      buffs:["Health", "Replenishment", "Armor Reduction(Minor)","Spell Damage %"],
     },
   ]
   
