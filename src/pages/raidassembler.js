a4df0ddb05bf68ea3f1fc6160e249a1ae5482a5c
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2021-11-11 22:19:48 +0100

    styles organized, made a drawer for classbuttons

--- src/pages/raidassembler.js
+++ src/pages/raidassembler.js
@@ -3,13 +3,23 @@ import SpecList from '../SpecList';
 import PlayersInRaid from '../components/PlayersInRaid';
 import { useState } from 'react';
 import RaidBuffs from '../components/RaidBuffs';
-import { Main, SpecContainer, RaidContainer, BuffContainer } from '../components/styles/RaidAssembler.styled';
+import { Main, SpecContainer, RaidContainer, BuffContainer} from '../components/styles/RaidAssembler.styled';
+import { FaBars } from "react-icons/fa";
+
 
 const RaidAssembler = () => {
 	const [ raid, setRaid ] = useState([]);
 	const [ buffs, setBuffs ] = useState([]);
 	const [ count, setCount ] = useState(0);
+	const [ specContainerOpen, setspecContainerOpen ] = useState(true);
 
+
+	const handleToggle = () => {
+		setspecContainerOpen(!specContainerOpen);
+		console.log(specContainerOpen)
+	};
+
+	
 	const deletePlayer = (id) => {
 		setRaid(raid.filter((player) => player.id !== id));
 		setBuffs(buffs.filter((playerBuffs) => playerBuffs.id !== id));
@@ -30,16 +40,18 @@ const RaidAssembler = () => {
 	};
 	return (
 		<Main>
-			<RaidContainer>
+			<RaidContainer className="raid-container">
 				<div className="count"> {count} / 25 </div>
 				<div className="grid">
 					{raid.length > 0 ? <PlayersInRaid raid={raid} onDelete={deletePlayer} /> : 'No Players in Raid'}
 				</div>
 			</RaidContainer>
-			<BuffContainer>{buffs.length > 0 ? <RaidBuffs buffs={buffs} /> : 'No Buffs in Raid'}</BuffContainer>
-			<SpecContainer>
-				<SpecButtons specs={SpecList} onClick={addPlayer} />
+			<SpecContainer className={`${specContainerOpen ? 'class-container' : 'hideSpecContainer'}`}>
+			<div onClick={handleToggle} className='toggle-button-container' > <FaBars className="toggle-show-button" /> </div>
+				<SpecButtons className='spec-buttons' specs={SpecList} onClick={addPlayer}/>
 			</SpecContainer>
+			<BuffContainer className="buff-container">{buffs.length > 0 ? <RaidBuffs buffs={buffs} /> : 'No Buffs in Raid'}</BuffContainer>
+
 		</Main>
 	);
 };
