3e9bd65405e15f5f1e29dc9dac9324246cc326bf
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2021-11-13 21:00:30 +0100

    ra-menu fixed

--- src/components/styles/RaidAssembler.styled.js
+++ src/components/styles/RaidAssembler.styled.js
@@ -1,80 +1,58 @@
 import styled from 'styled-components';
-//#7575FF
+import { CgChevronDoubleRightR } from "react-icons/cg";
+
 export const Main = styled.main`
-  display: grid; 
-  grid-template-columns: 1.6fr 0.4fr; 
-  grid-template-rows: 0.3fr 1.4fr; 
-  gap: 0px 0px; 
-  grid-template-areas: 
-    "raid-container spec-container"
-    "buff-container spec-container"; 
+	display:flex;
+	flex-direction:column;
+	align-items: center;
+	justify-content: start;
 	max-width: 100vw;
 	min-height: 100vw;
 	margin: auto;
-	min-height: 500px;
+	min-height: 100%;
 	padding: 20px;
 	margin-top: 10%;
 	background-color: #667292;
 	opacity: 90%;
-	
-	
-	.spec-container {
-		transition: all 300ms;
-	}	
-	.hide-spec-container {
-		transition: all 300ms;
-		transform:translate(50%);
-		opacity:10%;
-	
-	.toggle-spec-container {
-		opacity:100%;
-		color:red;
-	}
-	}
 
 
+	.ra-left-menu {
+		transition: all 300ms;
+		.toggle-spec-button {
+		transition: all 200ms;
+	}
+}
+	.hide-ra-left-menu {
+			transition: transform 300ms;
+			transform:translate(75%);
+
+	}
+	
 	@media (min-width: 1200px) { 
-		max-width: 75vw;
-		min-height: 75vw;
-		border-radius: 5px;
-		box-shadow: 3px 5px 10px -2px rgba(0, 0, 0, 0.6);
-		
-		.toggle-spec-container {
-		display:none;
-		color:white;
-	}
+	display: grid; 
+	grid-template-columns: 1.6fr 0.4fr; 
+	grid-template-rows: 0.3fr 1.4fr; 
+	gap: 0px 0px; 
+	grid-template-areas: 
+    "raid-container spec-container"
+    "buff-container spec-container"; 
+	max-width: 75vw;
+	min-height: 75vw;
+	border-radius: 5px;
+	box-shadow: 3px 5px 10px -2px rgba(0, 0, 0, 0.6);
+	
 
 	 }
 `;
 
-
 export const SpecContainer = styled.section`
+	display:flex;
+	flex-direction: row;
+	align-items: center;
 
-		grid-area: spec-container;
-		position: absolute;
-		top: 10%;
-	 	right:0;
-		border: none;
-		max-width:40%;
-		color:white;
-	 	.toggle-spec-container {
-			display:flex;
-			flex-direction: row;
-			align-items: center;
-			justify-content: space-between;
-			
-		 }
-		.toggle-spec-button {
-			width:2rem;
-			height:2rem;
-			display: flex;
-			background-color: white;
-		}
-		.spec-buttons {
-			width:100%;
-		}
 	 
-	@media (min-width: 1200px) { 
+	@media (min-width: 1200px) {
+	grid-area: spec-container; 
 	display: flex;
 	flex-direction: row;
 	position: static;
@@ -84,6 +62,7 @@ export const SpecContainer = styled.section`
 	
 `;
 export const StyledSpecButton = styled.button`
+
 	display: flex;
 	flex-direction: row;
 	width:100%;
@@ -107,8 +86,9 @@ export const StyledSpecButton = styled.button`
 		filter: saturate(120%);
 	}
 	img {
-		width: 1.3rem;
-		height: 1.3rem;
+		width: 0.8rem;
+		height: 0.8rem;
+		border-radius: 50%;
 	}
 
 
@@ -119,12 +99,14 @@ export const StyledSpecButton = styled.button`
 `;
 
 export const RaidContainer = styled.section`
+	display: flex;
 	grid-area: raid-container;
 	padding:1rem;
 	border-radius: .5rem;
 	background-color:white;
 	width:100%;
 	height:fit-content;
+	margin-bottom: 1rem;
 
 	.count {
 		font-size: 1em;
@@ -154,7 +136,7 @@ export const StyledPlayer = styled.div`
 	border: 1px solid #dcdcdc;
 	cursor: pointer;
 	color: #666666;
-	font-size: .7rem;
+	font-size: .5rem;
 	padding: .3rem;
 	text-shadow: 0px 1px 0px #ffffff;
 	transition: transform 0.2;
@@ -196,3 +178,7 @@ export const BuffContainer = styled.section`
 	padding:1rem;
 `;
 
+export const DoubleArrowRight = styled(CgChevronDoubleRightR)`
+		font-size:3rem;
+		color:white;
+	`
