76df05d6028319fd0e63923b4592c21e334d82f5
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2021-11-16 21:39:03 +0100

    initial commit

---
+++ src/AllBuffs.js
@@ -0,0 +1,134 @@
+const AllBuffs = [
+{
+    name:'Agility and Strength',
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Armor %',
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Armor Reduction(Major)', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Armor Reduction(Minor)', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Armor Reduction(Minor)', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+ 
+{
+    name:'Attack Power %', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Bloodlust', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Crit Strike %', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Damage %', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Damage Reduction %', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Disease Damage', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Haste %', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Healing Received %', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Health', 
+
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Intellect', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Mana per 5', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Melee Crit', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Melee Haste', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Motw', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Phys Vulnerability', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Replenishment', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Spell Crit', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Spell Damage %', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Spell Haste', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Spell Hit', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Spellpower', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Spellpower(Major)', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+{
+    name:'Spellpower(Minor)', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+ 
+{
+    name:'Spirit', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+ 
+{
+    name:'Stamina', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+ 
+{
+    name:'Stats 10%', 
+    buffs:['Horn of Winter', 'Strength of Earth Totem']
+},
+ 
+
+
+]
