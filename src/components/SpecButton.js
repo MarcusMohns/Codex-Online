d926179469573d2e96854665335f774556ada6f4
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2021-12-27 19:03:25 +0100

    Moved blessingcheckboxes to different file, cleaned up original code

--- src/components/SpecButton.js
+++ src/components/SpecButton.js
@@ -1,42 +1,15 @@
+import BlessingCheckboxes from "./BlessingCheckboxes";
 import { StyledSpecButton } from "./styles/RaidAssembler.styled";
 const SpecButton = ({ text, image, onClick, color }) => {
-  return color === "pink" ? ( // if paladin
+  return (
     <StyledSpecButton color={color} onClick={onClick}>
       <p>{text}</p>
       <img src={image} alt="spec icon" />
-      <div>
-        <input
-          type="checkbox"
-          name={text.replace(/\s+/g, "")}
-          id={`${text.replace(/\s+/g, "")}-bok-checkbox`}
-          value="Kings"
-          defaultChecked
-        />{" "}
-        <label htmlFor={`${text.replace(/\s+/g, "")}-bok-checkbox`}>BoK</label>
-        <input
-          type="checkbox"
-          name={text.replace(/\s+/g, "")}
-          id={`${text.replace(/\s+/g, "")}-bom-checkbox`}
-          value="Might"
-        />{" "}
-        <label htmlFor={`${text.replace(/\s+/g, "")}-bom-checkbox`}>BoM</label>
-        <input
-          type="checkbox"
-          name={text.replace(/\s+/g, "")}
-          id={`${text.replace(/\s+/g, "")}-bow-checkbox`}
-          value="Wisdom"
-        />{" "}
-        <label htmlFor={`${text.replace(/\s+/g, "")}-bow-checkbox`}>BoW</label>
-      </div>
-    </StyledSpecButton>
-  ) : (
-    <StyledSpecButton color={color} onClick={onClick}>
-      <p>{text}</p>
-      <img src={image} alt="spec icon" />
+      {color === "pink" && <BlessingCheckboxes text={text} />}
+      {/* // If paladin add checkboxes */}
     </StyledSpecButton>
   );
 };
-
 SpecButton.defaultProps = {
   image: require("../images/hunter_beastmastery.png").default,
   text: "missing spec",
