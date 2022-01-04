20c0bb782848eff571db9540f9a92d6a5fd2454d
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-01-04 22:05:14 +0100

    Changed back to <StyledPlayer>

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,11 @@
-import { StyledPlayer2, DeletePlayerIcon } from "./styles/RaidAssembler.styled";
+import { StyledPlayer, DeletePlayerIcon } from "./styles/RaidAssembler.styled";
 import { Draggable } from "react-beautiful-dnd";
 
 const Player = ({ player, onDelete, index }) => {
   return (
     <Draggable draggableId={player.id} index={index}>
       {(provided) => (
-        <StyledPlayer2
+        <StyledPlayer
           background={player.color}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
@@ -17,7 +17,7 @@ const Player = ({ player, onDelete, index }) => {
           />
           <p>{player.text}</p>
           <img id="class-img" src={player.image} alt="player in raid" />
-        </StyledPlayer2>
+        </StyledPlayer>
       )}
     </Draggable>
   );
