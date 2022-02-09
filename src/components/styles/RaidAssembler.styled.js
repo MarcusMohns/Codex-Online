af02e5b9416a47c0d2810e9e048886faad52d82f
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-02-09 22:19:07 +0100

    styled save menu additionally

--- src/components/styles/RaidAssembler.styled.js
+++ src/components/styles/RaidAssembler.styled.js
@@ -1125,6 +1125,8 @@ export const StyledSaveMenu = styled.div`
   .save-menu {
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
     width: 300px;
     height: 300px;
     background-color: black;
@@ -1139,9 +1141,12 @@ export const StyledSaveMenu = styled.div`
     .one-save {
       display: flex;
       flex-direction: row;
+      width: 100%;
 
       .save-text {
         color: white;
+        padding: 5px;
+        width: 100%;
       }
     }
   }
