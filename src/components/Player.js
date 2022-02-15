d90e6451328ac869c48ec4df42b0fb58d2ceb7fe
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-02-15 01:18:19 +0100

    future proofed raidrole

--- src/components/Player.js
+++ src/components/Player.js
@@ -54,7 +54,7 @@ const Player = ({
               <span className="placeholder">{player.text}</span>
             </label>
             <img id="class-img" src={player.image} alt="player in raid" />
-            <RaidRole classText={player.text} />
+            <RaidRole player={player} />
             {/* If warrior or paladin add the checkboxes to check what buffs they provide */}
             {(player.color === COLORS.paladin ||
               player.color === COLORS.warrior) && (
--- src/components/RaidRole.js
+++ src/components/RaidRole.js
@@ -2,24 +2,10 @@ const healerIcon = <img src="images/Healer_icon.png" alt="plus sign" />;
 const tankIcon = <img src="images/Tank_icon.png" alt="shield" />;
 const dpsIcon = <img src="images/DPS_icon.png" alt="sword" />;
 
-const tanks = [
-  "Protection Paladin",
-  "Blood Death Knight",
-  "Feral Druid",
-  "Protection Warrior",
-];
-const healers = [
-  "Discipline Priest",
-  "Holy Priest",
-  "Holy Paladin",
-  "Restoration Druid",
-  "Restoration Shaman",
-];
-
-const RaidRole = ({ classText }) => {
-  if (tanks.includes(classText)) {
+const RaidRole = ({ player }) => {
+  if (player.role === "tank") {
     return <div className="role-img">{tankIcon}</div>;
-  } else if (healers.includes(classText)) {
+  } else if (player.role === "healer") {
     return <div className="role-img">{healerIcon}</div>;
   } else {
     return <div className="role-img">{dpsIcon}</div>;
