c2bafe305bb2efb43639a64722aabb01d39ae070
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-02-26 01:12:04 +0100

    styled home added images and made responsive

--- src/components/styles/HomeStyled.js
+++ src/components/styles/HomeStyled.js
@@ -3,7 +3,7 @@ import COLORS from "../../components";
 
 export const Main = styled.main`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   box-sizing: border-box;
   align-items: flex-start;
   justify-content: space-around;
@@ -11,6 +11,12 @@ export const Main = styled.main`
   min-width: 100%;
   min-height: 80%;
   height: auto;
+
+  #gem-finder-container {
+    @media (min-width: 900px) {
+      flex-direction: row-reverse;
+    }
+  }
 `;
 
 export const HomeSection = styled.section`
@@ -19,14 +25,77 @@ export const HomeSection = styled.section`
   align-items: center;
   box-sizing: border-box;
   border-radius: 10px;
-  min-height: 100%;
-  margin-top: 50px;
+  width: 100%;
   height: 500px;
-  width: 80%;
   background-color: ${COLORS.fadedBlack};
   border: 1px solid rgba(255, 255, 255, 0.1);
   box-shadow: 3px 5px 10px -2px rgba(0, 0, 0, 0.6);
   background-image: url("/images/howling_fjord.png");
   background-repeat: no-repeat;
   background-position: top;
+  position: absolute;
+  top: 10%;
+  z-index: -1;
+`;
+
+export const PagePreviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  min-height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: ${COLORS.fadedBlack};
+  z-index: 10;
+
+  .content-preview-image {
+    border-radius: 5px;
+    background-color: ${COLORS.fadedBlack};
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    box-shadow: 3px 5px 10px -2px rgba(0, 0, 0, 0.6);
+    opacity: 80%;
+    max-width: 95%;
+    max-height: 100%;
+    margin-bottom: 20px;
+
+    @media (min-width: 900px) {
+      width: 100%;
+      height: auto;
+      margin: 15px;
+    }
+  }
+
+  @media (min-width: 900px) {
+    flex-direction: row;
+    margin-bottom: 0px;
+  }
+  @media (min-width: 1200px) {
+    padding: 2% 10rem;
+  }
+`;
+
+export const PagePreviewText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  color: ${COLORS.offWhite};
+  flex-grow: 1;
+  margin: 15px;
+`;
+
+export const PagePreviewHeader = styled.h2`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  justify-content: center;
+  color: ${COLORS.offWhite};
+  margin-bottom: 1rem;
+  font-size: 22px;
+
+  @media (min-width: 900px) {
+    align-items: start;
+  }
 `;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,76 @@
-import { Main, HomeSection } from "../components/styles/HomeStyled";
+import {
+  Main,
+  HomeSection,
+  PagePreviewText,
+  PagePreviewContainer,
+  PagePreviewHeader,
+} from "../components/styles/HomeStyled";
+
+const raidAssemblerImage = (
+  <img
+    src="/images/raid_assembler.png"
+    className="content-preview-image"
+    alt="plus sign"
+  />
+);
+const arenaPointCalcImage = (
+  <img
+    src="/images/arena_point_calc.png"
+    alt="shield"
+    className="content-preview-image"
+  />
+);
+const gemFinderImage = (
+  <img
+    src="/images/gemfinder.png"
+    alt="sword"
+    className="content-preview-image"
+  />
+);
+
 const Home = () => {
   return (
     <Main>
       <HomeSection />
+      <PagePreviewContainer>
+        <PagePreviewText>
+          <PagePreviewHeader>Raid Assembler</PagePreviewHeader>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </PagePreviewText>
+        {raidAssemblerImage}
+      </PagePreviewContainer>
+      <PagePreviewContainer id="gem-finder-container">
+        <PagePreviewText>
+          <PagePreviewHeader>Gem Finder</PagePreviewHeader>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </PagePreviewText>
+        {gemFinderImage}
+      </PagePreviewContainer>
+      <PagePreviewContainer>
+        <PagePreviewText>
+          <PagePreviewHeader>Arena Point Calculator</PagePreviewHeader>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </PagePreviewText>
+        {arenaPointCalcImage}
+      </PagePreviewContainer>
     </Main>
   );
 };
