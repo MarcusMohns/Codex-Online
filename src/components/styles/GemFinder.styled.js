79276551f340b06c87448534cbaf0912067d10c3
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-02-27 23:52:11 +0100

    hover color swapped

--- src/components/styles/GemFinder.styled.js
+++ src/components/styles/GemFinder.styled.js
@@ -234,7 +234,7 @@ export const GemTableContainer = styled.section`
     }
 
     &:hover {
-      background-color: ${COLORS.fadedHighlight};
+      background-color: ${COLORS.fadedBackground};
     }
   }
   .gemNameCell {
