8a86b6308997ad5581c9ae169b2d62000b7fd754
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-02-28 15:20:30 +0100

    addded some shadows to main

--- src/components/styles/HomeStyled.js
+++ src/components/styles/HomeStyled.js
@@ -11,6 +11,7 @@ export const Main = styled.main`
   min-width: 100%;
   min-height: 80%;
   height: auto;
+  box-shadow: 3px 5px 10px -2px rgba(255, 255, 255, 0.2);
 
   #gem-finder-container {
     @media (min-width: 900px) {
--- src/components/styles/RaidAssembler.styled.js
+++ src/components/styles/RaidAssembler.styled.js
@@ -34,6 +34,7 @@ export const Main = styled.main`
       "Buffs Buffs";
     padding: 2rem;
     padding-bottom: 10rem;
+    box-shadow: 3px 5px 10px -2px rgba(0, 0, 0, 0.6);
   }
   .add-player-menu {
     display: flex;
