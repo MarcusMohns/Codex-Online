7b0eba924d37f33a1c0dff1582f4f056202158e6
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-03-04 01:17:29 +0100

    added discord link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import COLORS from "../components";
 import styled from "styled-components";
+import { RiDiscordFill } from "@react-icons/all-files/ri/RiDiscordFill";
 
 const StyledFooter = styled.footer`
   display: flex;
@@ -9,7 +10,24 @@ const StyledFooter = styled.footer`
   height: 75px;
   background-color: ${COLORS.fadedGrey};
   box-shadow: 0 0 0.2rem ${COLORS.fadedGrey};
-  color: white;
+  color: grey;
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-style: normal;
+    text-decoration: none;
+    color: white;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const DiscordIcon = styled(RiDiscordFill)`
+  font-size: 2rem;
 `;
 
 const Footer = () => {
@@ -18,7 +36,11 @@ const Footer = () => {
       <div>Copyright</div>
       <div>Contact</div>
       <div>About</div>
-      <a href="https://discord.gg/JskSjDrrwh">Connect</a>
+
+      <a href="https://discord.gg/JskSjDrrwh" target="blank">
+        <DiscordIcon />
+        Connect
+      </a>
     </StyledFooter>
   );
 };
