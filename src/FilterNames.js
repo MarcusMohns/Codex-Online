8e9cce6069aea0989e593c3567ff32d3d4423464
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-03-23 00:42:40 +0100

    added meta gems

--- src/FilterNames.js
+++ src/FilterNames.js
@@ -1,37 +1,37 @@
 const FilterNames = [
-	{
-		name: 'Main Attributes',
-		content: [ 'Agility', 'Intellect', 'Spirit', 'Stamina', 'Strength' ]
-	},
+  {
+    name: "Main Attributes",
+    content: ["Agility", "Intellect", "Spirit", "Stamina", "Strength"],
+  },
 
-	{
-		name: 'Secondary Attributes',
-		content: [
-			'Armor Pen',
-			'Attack Power',
-			'Critical Strike',
-			'Defense',
-			'Dodge',
-			'Expertise',
-			'Haste',
-			'Hit',
-			'Mp5',
-			'Parry',
-			'Resilience',
-			'Spell Pen',
-			'Spell Power'
-		]
-	},
+  {
+    name: "Secondary Attributes",
+    content: [
+      "Armor Pen",
+      "Attack Power",
+      "Critical Strike",
+      "Defense",
+      "Dodge",
+      "Expertise",
+      "Haste",
+      "Hit",
+      "Mp5",
+      "Parry",
+      "Resilience",
+      "Spell Pen",
+      "Spell Power",
+    ],
+  },
 
-	{
-		name: 'Colors',
-		content: [ 'Blue', 'Red', 'Yellow', 'Orange', 'Green', 'Purple' ]
-	},
+  {
+    name: "Colors",
+    content: ["Blue", "Red", "Yellow", "Orange", "Green", "Purple"],
+  },
 
-	{
-		name: 'Quality',
-		content: [ 'Uncommon', 'Perfect', 'Rare', 'Epic' ]
-	}
+  {
+    name: "Quality",
+    content: ["Uncommon", "Perfect", "Rare", "Epic", "Meta"],
+  },
 ];
 
 export default FilterNames;
