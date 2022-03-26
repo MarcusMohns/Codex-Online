95a9aa1b0c8e4f41410777de9593a65c482cac48
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-03-26 22:42:31 +0100

    Optimized raidcooldowns creation, no longer multiple iterations

--- src/components/RaidCooldowns.js
+++ src/components/RaidCooldowns.js
@@ -1,7 +1,12 @@
-import React from "react";
-import { StyledRaidCooldowns } from "./styles/RaidAssembler.styled";
+import React, { memo } from "react";
+import {
+  ManaIcon,
+  CooldownIcon,
+  StyledRaidCooldowns,
+  PowerUpIcon,
+  CombatResIcon,
+} from "./styles/RaidAssembler.styled";
 import Utilities from "./Utilities";
-import { memo } from "react";
 
 const RaidCooldowns = ({
   setRaidCooldownsOpen,
@@ -9,8 +14,6 @@ const RaidCooldowns = ({
   utilities,
 }) => {
   const raidCooldowns = [
-    "Rebirth",
-    "Innervate",
     "Tranquility",
     "Divine Sacrifice",
     "Hand of Sacrifice",
@@ -18,17 +21,30 @@ const RaidCooldowns = ({
     "Aura Mastery",
     "Divine Hymn",
     "Pain Suppression",
-    "Mana Tide Totem",
-    "Hymn of Hope",
   ];
-
+  const ManaCooldowns = ["Mana Tide Totem", "Hymn of Hope", "Innervate"];
+  const CombatResCooldowns = ["Rebirth"];
+  const PowerUpCooldowns = ["Hysteria", "Power Infusion", "Bloodlust/Heroism"];
   const raidUtilities = Object.values(utilities);
-  const ourRaidCooldowns = raidUtilities.map((ourUtilities) =>
-    ourUtilities.filter(
-      // Filter out spells that are not raidCooldowns but keep structure the same as utilities so we can pass it into existing <Utilities>
-      (utility) => raidCooldowns.includes(utility.name) && utility
-    )
-  );
+
+  const ourRaidCooldowns = [];
+  const ourManaCooldowns = [];
+  const ourBattleResCooldowns = [];
+  const ourPowerUpCooldowns = [];
+
+  for (let ourUtilities of raidUtilities) {
+    for (let utility of ourUtilities) {
+      if (raidCooldowns.includes(utility.name)) {
+        ourRaidCooldowns.push([utility]);
+      } else if (ManaCooldowns.includes(utility.name)) {
+        ourManaCooldowns.push([utility]);
+      } else if (CombatResCooldowns.includes(utility.name)) {
+        ourBattleResCooldowns.push([utility]);
+      } else if (PowerUpCooldowns.includes(utility.name)) {
+        ourPowerUpCooldowns.push([utility]);
+      }
+    }
+  }
 
   return (
     <StyledRaidCooldowns
@@ -36,7 +52,26 @@ const RaidCooldowns = ({
     >
       <div className="cooldown-panel" onClick={(e) => e.stopPropagation()}>
         <h2 className="cooldown-header">Raid Cooldowns</h2>
+        <h3 className="cooldown-subheader">
+          <CooldownIcon />
+          Damage Reduction & Healing
+        </h3>
         <Utilities utilities={ourRaidCooldowns} />
+        <h3 className="cooldown-subheader">
+          <ManaIcon />
+          Mana Cooldowns
+        </h3>
+        <Utilities utilities={ourManaCooldowns} />
+        <h3 className="cooldown-subheader">
+          <PowerUpIcon />
+          Powerup Cooldowns
+        </h3>
+        <Utilities utilities={ourPowerUpCooldowns} />
+        <h3 className="cooldown-subheader">
+          <CombatResIcon />
+          Combat Ressurections
+        </h3>
+        <Utilities utilities={ourBattleResCooldowns} />
       </div>
     </StyledRaidCooldowns>
   );
