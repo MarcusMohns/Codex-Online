8a8e284f83c84c0f885efb623469b25c77d2b790
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-06-14 16:25:03 +0200

    renamed raid assembler raid helper

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,8 +13,8 @@ const Home = () => {
       <HomeSection />
       <PagePreviewContainer id="raid-assembler-container">
         <PagePreviewText>
-          <PagePreviewHeader>Raid Assembler</PagePreviewHeader>
-          Organize 3.3.5 WOTLK Buffs, Debuffs and Utilities. The Raid Assembler
+          <PagePreviewHeader>Raid Helper</PagePreviewHeader>
+          Organize 3.3.5 WOTLK Buffs, Debuffs and Utilities. The Raid Helper
           helps you optimize your raids performance, manage the players in your
           raid, monitor the raids buffs, utilities and roles and save your work
           in the browser or locally in order to share with others!
