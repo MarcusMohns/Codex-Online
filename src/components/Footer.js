399101d750cebddb87f8a7853de8b2dc10802a28
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-08-03 19:04:18 +0200

    fixed odd footer behavior

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const StyledFooter = styled.footer`
   box-shadow: 0 0 0.2rem ${COLORS.fadedGrey};
   color: grey;
   font-size: 0.9rem;
-  margin-top: 14rem;
+  margin-top: auto;
 
   a {
     display: flex;
