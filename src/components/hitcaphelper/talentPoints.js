2586ea6307227c0ac11722954bbd220ec8789f26
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-11-07 23:59:43 +0100

    added our switch statement

--- src/components/hitcaphelper/talentPoints.js
+++ src/components/hitcaphelper/talentPoints.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const TalentPoints = ({ nextStep, prevStep, handleChange, values }) => {
+  console.log(values);
   const next = (e) => {
     e.preventDefault();
     nextStep();
@@ -9,13 +10,21 @@ const TalentPoints = ({ nextStep, prevStep, handleChange, values }) => {
     e.preventDefault();
     prevStep();
   };
-  return (
-    <div>
-      <h1>TalentPoints</h1>
-      <input onClick={previous} type="button" value="previous" />
-      <input onClick={next} type="button" value="next" />
-    </div>
-  );
+
+  switch (values.classAndSpec) {
+    case "Shadow Priest":
+      return <div>Shadow Priest</div>;
+    case "Holy Priest":
+      return <div>Shadow Priest</div>;
+    default:
+      return (
+        <div>
+          <h1>TalentPoints</h1>
+          <input onClick={previous} type="button" value="previous" />
+          <input onClick={next} type="button" value="next" />
+        </div>
+      );
+  }
 };
 
 export default TalentPoints;
