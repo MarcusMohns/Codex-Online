ea79ff89e9b0052cfc643b4a155e23ade5f3c9ff
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-11-27 22:17:01 +0100

    displaying results

--- src/components/hitcaphelper/Result.js
+++ src/components/hitcaphelper/Result.js
@@ -6,8 +6,13 @@ const Result = ({ prevStep, handleChange, values }) => {
     e.preventDefault();
     prevStep();
   };
+  const ourValues = Object.entries(values).map(([key, value]) => {
+    return <div key={key}>{value}</div>;
+  });
+
   return (
     <div>
+      {ourValues}
       <h1>Result</h1>
       <input onClick={previous} type="button" value="previous" />
     </div>
