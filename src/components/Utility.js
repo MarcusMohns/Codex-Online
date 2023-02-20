94a76618d5e6c09cc590f394480a9decc5411086
Marcus Mohns <92151780+MarcusMohns@users.noreply.github.com>
2023-02-20 16:44:36 +0100

    show wowhead tooltip on hover

--- src/components/Utility.js
+++ src/components/Utility.js
@@ -1,8 +1,13 @@
 import { StyledUtility } from "./styles/RaidAssembler.styled";
 
 const Utility = ({ utility }) => {
+  console.log(utility);
   return (
-    <StyledUtility key={utility.name}>
+    <StyledUtility
+      key={utility.name}
+      href={utility.link}
+      data-wowhead={`item-${utility.id}`}
+    >
       {utility.count}x {utility.name}
       <img src={utility.image} alt="utility spell, e.g misdirection" />
     </StyledUtility>
@@ -13,6 +18,8 @@ Utility.defaultProps = {
   image: "images/hunter_beastmastery.png",
   name: "missing spec",
   count: 0,
+  spellId: "57330",
+  link: "https://www.wowhead.com/wotlk/spell=57330/horn-of-winter",
 };
 
 export default Utility;
