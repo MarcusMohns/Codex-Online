bc38e9d67635585837d4b9ebe546a17c9286d0d1
Marcus Mohns <92151780+MarcusMohns@users.noreply.github.com>
2023-03-02 15:18:01 +0100

    move initial states to data

---
+++ src/data/InitialStates.js
@@ -0,0 +1,44 @@
+export const intitialRaidState = {
+  players: [],
+  groups: {
+    "group-1": {
+      id: "group-1",
+      title: "Group 1",
+      playerIds: [],
+    },
+    "group-2": {
+      id: "group-2",
+      title: "Group 2",
+      playerIds: [],
+    },
+    "group-3": {
+      id: "group-3",
+      title: "Group 3",
+      playerIds: [],
+    },
+    "group-4": {
+      id: "group-4",
+      title: "Group 4",
+      playerIds: [],
+    },
+    "group-5": {
+      id: "group-5",
+      title: "Group 5",
+      playerIds: [],
+    },
+  },
+  groupOrder: ["group-1", "group-2", "group-3", "group-4", "group-5"],
+};
+
+export const initialPlayerState = {
+  id: "6439cdb9-19a2-44d4-90fc-3ebab48382fe",
+  text: "Retribution Paladin",
+  image: "images/Retribution_Paladin.webp",
+  icon: "images/classicons/Retribution_Paladin.webp",
+  color: "rgb(244,140,186)",
+  role: "dps",
+  name: "",
+  buffs: [],
+  utility: [],
+  playerBuffs: [],
+};
--- src/pages/raidhelper/raidhelper.js
+++ src/pages/raidhelper/raidhelper.js
@@ -8,6 +8,8 @@ import RaidCooldowns from "./components/RaidCooldowns";
 import PlayersIndex from "./components/PlayersIndex";
 import PlayerOptions from "./components/PlayerOptions";
 import Raid from "./components/Raid";
+import { intitialRaidState } from "../../data/InitialStates";
+import { initialPlayerState } from "../../data/InitialStates";
 import {
   Main,
   SpecContainer,
@@ -69,38 +71,6 @@ const formReducer = (state, action) => {
 };
 
 const RaidHelper = () => {
-  const intitialRaidState = {
-    players: [],
-    groups: {
-      "group-1": {
-        id: "group-1",
-        title: "Group 1",
-        playerIds: [],
-      },
-      "group-2": {
-        id: "group-2",
-        title: "Group 2",
-        playerIds: [],
-      },
-      "group-3": {
-        id: "group-3",
-        title: "Group 3",
-        playerIds: [],
-      },
-      "group-4": {
-        id: "group-4",
-        title: "Group 4",
-        playerIds: [],
-      },
-      "group-5": {
-        id: "group-5",
-        title: "Group 5",
-        playerIds: [],
-      },
-    },
-    groupOrder: ["group-1", "group-2", "group-3", "group-4", "group-5"],
-  };
-
   const [raid, setRaid] = useState(intitialRaidState);
   const [buffs, setBuffs] = useReducer(formReducer, {});
   const [utilities, setUtilities] = useReducer(formReducer, {});
@@ -110,18 +80,7 @@ const RaidHelper = () => {
   const [raidCooldownsOpen, setRaidCooldownsOpen] = useState(false);
   const [playersIndexOpen, setPlayersIndexOpen] = useState(false);
   const [playerOptionsOpen, setPlayerOptionsOpen] = useState(false);
-  const [aPlayer, setAPlayer] = useState({
-    id: "6439cdb9-19a2-44d4-90fc-3ebab48382fe",
-    text: "Retribution Paladin",
-    image: "images/Retribution_Paladin.webp",
-    icon: "images/classicons/Retribution_Paladin.webp",
-    color: "rgb(244,140,186)",
-    role: "dps",
-    name: "",
-    buffs: [],
-    utility: [],
-    playerBuffs: [],
-  });
+  const [aPlayer, setAPlayer] = useState(initialPlayerState);
 
   const resetRaid = () => {
     setRaid(intitialRaidState);
