8a747d471b6a4b5b6b0395bc06cedafc2ff5bd68
Marcus Mohns <92151780+MarcusMohns@users.noreply.github.com>
2023-03-07 16:44:05 +0100

    separate file for checkbox

---
+++ src/pages/raidhelper/components/PlayerOptionsCheckbox.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const OptionsCheckbox = ({ buff, player, editBuffs }) => {
+  return (
+    <div className="player-checkbox-container">
+      <label className="player-checkbox" htmlFor={`${buff.name}-${player.id}`}>
+        <div className="player-checkbox-text">{buff.name}</div>
+
+        <input
+          type="checkbox"
+          id={`${buff.name}-${player.id}`}
+          className="player-checkbox-input"
+          defaultChecked={buff.checked}
+          onClick={(e) => editBuffs(player, buff, e)}
+        />
+        <span className="styled-player-checkmark"></span>
+        <img src={buff.image} alt="buff" className="player-checkbox-image" />
+      </label>
+    </div>
+  );
+};
+
+export default OptionsCheckbox;
