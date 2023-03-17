d8a659066c4127e3600a03c9316025c279a5c414
Marcus Mohns <92151780+MarcusMohns@users.noreply.github.com>
2023-03-17 10:33:01 +0100

    add color

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,6 @@
 const COLORS = {
   fadedBlack: "rgba(0, 0, 0, 0.5)",
+  veryFadedBlack: "rgba(0, 0, 0, 0.4)",
   greyText: "rgba(102, 102, 102, 1)",
   lightText: "#CEDDEC",
   offWhite: "rgba(248, 248, 255, 1)",
