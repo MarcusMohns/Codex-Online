8e817ec552ad6bf3999023ae1296d764ce7a3b7d
Marcus Mohns <92151780+MarcusMohns@users.noreply.github.com>
2023-03-19 23:10:11 +0100

    add undead and updated additional hit table

--- src/pages/hitcaphelper/components/Tables.js
+++ src/pages/hitcaphelper/components/Tables.js
@@ -4,59 +4,54 @@ export const additionalHitTable = (
   <table className="additional-hit-table">
     <thead>
       <tr>
-        <th>
-          <span>Race/Spec</span>
-        </th>
-        <th>
-          <span>Spells</span>
-        </th>
-        <th>
-          <span>Melee</span>
-        </th>
-        <th>
-          <span>Ranged</span>
-        </th>
-        <th>
-          <span>Nature Spells</span>
-        </th>
+        <th>Race/Spec</th>
+        <th>Spells</th>
+        <th>Melee</th>
+        <th>Ranged</th>
+        <th>Nature Spells</th>
+        <th>Shadow Spells</th>
       </tr>
     </thead>
     <tbody>
       <tr>
         <td>Blood Elf</td>
-        <td>
-          <span>2% (53 hit rating)</span>
-        </td>
-        <td>
-          <span>0%</span>
-        </td>
-        <td>
-          <span>0%</span>
-        </td>
-        <td>
-          <span>0%</span>
-        </td>
+        <td>2% (53 hit rating)</td>
+        <td>0%</td>
+        <td>0%</td>
+        <td>2% (53 hit rating)</td>
+        <td>2% (53 hit rating)</td>
       </tr>
       <tr>
         <td>Night Elf</td>
         <td>0%</td>
         <td>2% (66 hit rating)</td>
         <td>2% (66 hit rating)</td>
-        <td>2% (53 hit rating)</td>
+        <td>0%</td>
+        <td>0%</td>
       </tr>
       <tr>
         <td>Retribution Paladin</td>
         <td>4% (105 hit rating)</td>
         <td>0%</td>
         <td>4% (132 hit rating)</td>
-        <td>0%</td>
+        <td>4% (105 hit rating)</td>
+        <td>4% (105 hit rating)</td>
       </tr>
       <tr>
         <td>Sublety Rogue</td>
         <td>4% (105 hit rating)</td>
         <td>0%</td>
         <td>4% (132 hit rating)</td>
+        <td>4% (105 hit rating)</td>
+        <td>4% (105 hit rating)</td>
+      </tr>
+      <tr>
+        <td>Undead</td>
         <td>0%</td>
+        <td>0%</td>
+        <td>0%</td>
+        <td>0%</td>
+        <td>2% (53 hit rating)</td>
       </tr>
     </tbody>
   </table>
