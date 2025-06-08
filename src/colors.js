0c0a2b30a1f58faa55a0964dd377a442988be0e5
Marcus Mohns <marcus@mohns.se>
2025-06-08 21:55:04 +0200

    Update color values to not use opacity

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,5 @@
 const COLORS = {
-  fadedBlack: "rgba(0, 0, 0, 0.5)",
+  fadedBlack: "#161718",
   veryFadedBlack: "rgba(0, 0, 0, 0.4)",
   greyText: "rgba(102, 102, 102, 1)",
   greyBg: "rgba(26, 27, 27, 1)",
@@ -7,7 +7,7 @@ const COLORS = {
   offWhite: "rgba(248, 248, 255, 1)",
   fadedOffWhite: "rgba(248, 248, 255, 0.7)",
   background: "rgba(99, 102, 106, 1)",
-  fadedBackground: "rgba(99, 102, 106, 0.3)",
+  fadedBackground: "#1b1c1d",
   primary: "rgba(41, 61, 90, 1)",
   darkPrimary: "rgba(31, 60, 97, 1)",
   lightPrimary: "rgba(50, 94, 151, 1)",
